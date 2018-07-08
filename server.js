f97eef8f788dd05588bab5760670ccab0fcd9ef2
anumehaagrawal <anuzenith29@gmail.com>
2018-07-08 10:11:27 +0530

    fixed

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-var conString = "mongodb://anumeha:systers2018@ds217131.mlab.com:17131/demo-inviter"
-
+var conString = "mongodb://anumeha:systers2018@ds217131.mlab.com:17131/demo-inviter";
 var express = require("express");
 var app = express();
 var port = 3000;
@@ -10,7 +9,7 @@ var nodeMailer = require('nodemailer');
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
  mongoose.connect(conString, { useMongoClient: true }, () => {
-    console.log("DB is connected");
+    
 });
  var nameSchema = new mongoose.Schema({
     name: String,
@@ -32,32 +31,30 @@ app.post("/addname", (req, res) => {
             res.status(400).send("Unable to save to database");
         });
         let transporter = nodeMailer.createTransport({
-          host: 'smtp.gmail.com',
+          host: "smtp.gmail.com",
           port: 465,
           secure: true,
           auth: {
-              user: 'anumehaagrawal29@gmail.com',
-              pass: ''
+              user: "anumehaagrawal29@gmail.com",
+              pass: ""
           }
       });
       let mailOptions = {
-          from: '"Anuuzen" <anumehaagrawal29@gmail.com>', // sender address
+          from: "<anumehaagrawal29@gmail.com>", // sender address
           to: req.body.email, // list of receivers
           subject: req.body.name, // Subject line
-          text: 'Hell', // plain text body
-          html: '<b>NodeJS Email Tutorial</b>' // html body
+          text: "Hell", // plain text body
+          html: "<b>NodeJS Email Tutorial</b>" // html body
       };
 
       transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-              return console.log(error);
-          }
-          console.log('Message %s sent: %s', info.messageId, info.response);
+          if (!error) {
               res.render('index');
+            }
           });
 
 });
 
 app.listen(port, () => {
-    console.log("Server listening on port " + port);
+    
 });
