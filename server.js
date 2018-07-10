15d96aa0a0df1fd89f774dba97d14d1222e25e65
anumehaagrawal <anuzenith29@gmail.com>
2018-07-10 14:01:06 +0530

    Basic UI added

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post("/addname", (req, res) => {
     var myData = new User(req.body);
     myData.save()
         .then(item => {
-            res.send("Name saved to database");
+            res.sendFile(__dirname + "/response.html");
         })
         .catch(err => {
             res.status(400).send("Unable to save to database");
