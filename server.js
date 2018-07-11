14329f44ce0da321c75114dfd2d6cd3c5388099b
anumehaagrawal <anuzenith29@gmail.com>
2018-07-11 23:57:12 +0530

    Template added

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post("/addname", (req, res) => {
           to: req.body.name, // list of receivers
           subject: "Join the GHC slack channel !", // Subject line
           text: "Hell", // plain text body
-          html: '<img src ="http://abetteruserexperience.com/wp-content/uploads/2016/07/slack.png" > <br> <br><br><h1> Join GHC18 on Slack</h1><br><img src="https://ci5.googleusercontent.com/proxy/fyAoFvPyo0eYUKe4_VolitANLFrpYnGlA9dPRAn9Thn2P-RtFZBrniNkAu7FEmEg_j-XJ5-p40WabZ2smDC0Ava2DFzgnTXUyxPRRi5doXPnr-vKqfniYrZ7BdVMgjXh-2uPUhhLM3EK5qWWF6YjgV0n2yqQ0Q0u8Om6v6mWM0eh=s0-d-e1-ft#https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2017-10-03/251165127045_b5f26f2478857e3c6557_88.jpg"><a href="https://ghc17.slack.com/"><h3>ghc 18</h3></a>'
+          html: '<img src ="http://abetteruserexperience.com/wp-content/uploads/2016/07/slack.png" > <br> <br><br><h1> Join GHC18 on Slack</h1><br><img src="https://ci5.googleusercontent.com/proxy/fyAoFvPyo0eYUKe4_VolitANLFrpYnGlA9dPRAn9Thn2P-RtFZBrniNkAu7FEmEg_j-XJ5-p40WabZ2smDC0Ava2DFzgnTXUyxPRRi5doXPnr-vKqfniYrZ7BdVMgjXh-2uPUhhLM3EK5qWWF6YjgV0n2yqQ0Q0u8Om6v6mWM0eh=s0-d-e1-ft#https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2017-10-03/251165127045_b5f26f2478857e3c6557_88.jpg"><a href="https://ghc17.slack.com/"><h2>GHC 2018</h2></a>'
       };
 
       transporter.sendMail(mailOptions, (error, info) => {
