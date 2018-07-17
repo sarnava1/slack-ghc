57614e20540cd68c9ca58db7a512d15a22ad6ae5
anumehaagrawal <anuzenith29@gmail.com>
2018-07-18 00:16:06 +0530

    fixes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var nodeMailer = require('nodemailer');
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
- mongoose.connect(conString, { useMongoClient: true }, () => {
+ mongoose.connect(conString,  () => {
     
 });
  var nameSchema = new mongoose.Schema({
@@ -44,7 +44,7 @@ app.post("/addname", (req, res) => {
           to: req.body.name, // list of receivers
           subject: "Join the GHC slack channel !", // Subject line
           text: "Hell", // plain text body
-          html: '<img src ="http://abetteruserexperience.com/wp-content/uploads/2016/07/slack.png" > <br> <br><br><h1> Join GHC18 on Slack</h1><br><img src="https://ci5.googleusercontent.com/proxy/fyAoFvPyo0eYUKe4_VolitANLFrpYnGlA9dPRAn9Thn2P-RtFZBrniNkAu7FEmEg_j-XJ5-p40WabZ2smDC0Ava2DFzgnTXUyxPRRi5doXPnr-vKqfniYrZ7BdVMgjXh-2uPUhhLM3EK5qWWF6YjgV0n2yqQ0Q0u8Om6v6mWM0eh=s0-d-e1-ft#https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2017-10-03/251165127045_b5f26f2478857e3c6557_88.jpg"><a href="https://ghc17.slack.com/"><h2>GHC 2018</h2></a>'
+          html: '<img src ="http://abetteruserexperience.com/wp-content/uploads/2016/07/slack.png" > <br> <br><br><h1> Join GHC18 on Slack</h1><br><img src="https://ci5.googleusercontent.com/proxy/fyAoFvPyo0eYUKe4_VolitANLFrpYnGlA9dPRAn9Thn2P-RtFZBrniNkAu7FEmEg_j-XJ5-p40WabZ2smDC0Ava2DFzgnTXUyxPRRi5doXPnr-vKqfniYrZ7BdVMgjXh-2uPUhhLM3EK5qWWF6YjgV0n2yqQ0Q0u8Om6v6mWM0eh=s0-d-e1-ft#https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2017-10-03/251165127045_b5f26f2478857e3c6557_88.jpg"><a href="https://ghc17.slack.com/"><h2>Click here to join the slack channel</h2></a>'
       };
 
       transporter.sendMail(mailOptions, (error, info) => {
